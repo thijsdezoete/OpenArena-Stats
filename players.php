bcdf9f5bf076842b3215d91492a5c62502ce69c9
Jeroen Seegers <jeroen.seegers@spilgames.com>
2012-04-27 22:52:14 +0200

    Players file re-added

---
+++ players.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * In this array you can specify real names for the players you want to get statistics from.
+ *
+ * For example:
+ * $know_players = array(
+ *     'Acme' => 'John Doe',
+ *     'Grub' => 'Bantha Fodder'
+ * );
+ */
+$known_players = array(
+    'Aecrim'        => 'Mircea',
+    'Revell'        => 'Jeroen',
+    'Enrique'       => 'Enrique',
+    'dennis'        => 'Dennis',
+    'Schoende'      => 'Sven',
+    'Archangel'     => 'Sven',
+    'TT'            => 'Thijs T',
+    'Thz'           => 'Thijs Z',
+    'Sitting Duck'  => 'Martin',
+    'Tamas'         => 'Tamas',
+    'Rolph'         => 'Rolph',
+    'Gerb'          => 'Gerben',
+);
+?>
